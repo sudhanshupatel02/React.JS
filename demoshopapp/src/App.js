8ce0437c062b625357e52dcae9c44dcbc4c701f8
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-21 15:25:13 +0530

    React-Basic 2

--- demoshopapp/src/App.js
+++ demoshopapp/src/App.js
@@ -3,17 +3,37 @@ import Item from './components/Item';
 import ItemDate from './components/ItemDate';
 
 function App() {
+  const response =[
+    {
+      ItemName: 'HareRam',
+      ItemDate: '09',
+      ItemMonth: 'May',
+      Itemyear: '2000'
+    },
+    {
+      ItemName: 'HareKrishan',
+      ItemDate: '20',
+      ItemMonth: 'June',
+      Itemyear: '1998'
+    },
+    {
+      ItemName: 'HareRam',
+      ItemDate: '24',
+      ItemMonth: 'Septembar',
+      Itemyear: '1975'
+    },
+  ];
   const itemTwoName = 'RamRam'
   return (
    <div>
-     <Item name = 'HareRam'></Item>
-    <ItemDate day = '09' month = ' May ' year = '2000'></ItemDate>
+     <Item name = {response[0].ItemName}></Item>
+    <ItemDate day = {response[0].ItemDate} month = {response[0].ItemMonth} year = {response[0].Itemyear}></ItemDate>
 
-     <Item name = 'HareKrishan'></Item>
-    <ItemDate day = '20' month = ' june ' year = '2005'></ItemDate>
+     <Item name = {response[1].ItemName}></Item>
+    <ItemDate day = {response[1].ItemDate} month = {response[1].ItemMonth} year = {response[1].Itemyear}></ItemDate>
 
-     <Item name = {itemTwoName}></Item>
-    <ItemDate day = '23' month = ' Septembar ' year = '2020'></ItemDate>
+     <Item name = {response[2].ItemName}></Item>
+    <ItemDate day = {response[2].ItemDate} month = {response[2].ItemMonth} year = {response[2].Itemyear}></ItemDate>
     <div className="App"> Hello world</div>
    </div>
    
