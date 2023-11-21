f365333a2bedb7e0aaa9a761de94fa6e9c610aa0
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-21 15:07:46 +0530

    React-Basic 2

--- demoshopapp/src/App.js
+++ demoshopapp/src/App.js
@@ -3,6 +3,7 @@ import Item from './components/Item';
 import ItemDate from './components/ItemDate';
 
 function App() {
+  const itemTwoName = 'RamRam'
   return (
    <div>
      <Item name = 'HareRam'></Item>
@@ -11,7 +12,7 @@ function App() {
      <Item name = 'HareKrishan'></Item>
     <ItemDate day = '20' month = ' june ' year = '2005'></ItemDate>
 
-     <Item name = 'RamRam'></Item>
+     <Item name = {itemTwoName}></Item>
     <ItemDate day = '23' month = ' Septembar ' year = '2020'></ItemDate>
     <div className="App"> Hello world</div>
    </div>
