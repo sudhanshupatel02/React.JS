4ba4fbd1ec96a50e21ddc4b60e846191d46594b4
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-21 14:02:42 +0530

    React-Basic 2

--- demoshopapp/src/App.js
+++ demoshopapp/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Item from './components/Item';
+import ItemDate from './components/ItemDate';
 
 function App() {
   return (
    <div>
      <Item></Item>
+    <ItemDate></ItemDate>
     <div className="App"> Hello world</div>
    </div>
    
---
+++ demoshopapp/src/components/ItemDate.js
@@ -0,0 +1,12 @@
+import './ItemDate.css';
+
+function ItemDate(){
+   return (<div className='date'>
+       <span>02</span>
+       <span>Feb</span>
+       <span>1997</span>
+   </div>);
+
+}
+
+export default ItemDate;
