e4c67e9c0dda8254c0ac5bf235b94b79a67e1862
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-23 14:25:01 +0530

    React Basic 2

--- demoshopapp/src/components/ProductItem.js
+++ demoshopapp/src/components/ProductItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react'; //!{useState} = React Hooks
 
 import ProductDate from './ProductDate';
 import Card from './Card';
@@ -6,7 +6,11 @@ import './ProductItem.css';
 
 const ProductItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+
    function clickHandler(){
+    // title = 'Papcon'; onclick karne se tital nhi change ho to ham yaha use karte hai [state] ko 
+    setTitle('  Popcons'); // yah shi hai
     console.log('button clicked');
    }
 
@@ -14,7 +18,7 @@ const ProductItem = (props) => {
     <Card className='product-item'>
       <ProductDate date={props.date} />
       <div className='product-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <button onClick={clickHandler}>Add to card</button>
     </Card>
