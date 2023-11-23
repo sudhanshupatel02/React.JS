1f5be287428f46fdb09886fbcd483d162c9faafc
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-23 13:44:33 +0530

    React Basic 2

--- demoshopapp/src/components/ProductItem.js
+++ demoshopapp/src/components/ProductItem.js
@@ -5,12 +5,18 @@ import Card from './Card';
 import './ProductItem.css';
 
 const ProductItem = (props) => {
+
+   function clickHandler(){
+    console.log('button clicked');
+   }
+
   return (
     <Card className='product-item'>
       <ProductDate date={props.date} />
       <div className='product-item__description'>
         <h2>{props.title}</h2>
       </div>
+      <button onClick={clickHandler}>Add to card</button>
     </Card>
   );
 }
