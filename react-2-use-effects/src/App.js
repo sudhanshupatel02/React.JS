e48745d7a7f7f408454afbfb8ac8bfbe7ae2ce67
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-25 14:38:48 +0530

    React Basic 2 use Effect

--- react-2-use-effects/src/App.js
+++ react-2-use-effects/src/App.js
@@ -25,6 +25,14 @@ function App() {
     console.log("UI Renderer changed");
   }, [text]);
 
+  // Variation4 - To Handler Unmounting of Component  
+  useEffect(() => {
+    console.log("Added");
+    return () => {
+      console.log("Removed");
+    }
+  }, [text]);
+
   return (
     <div className="App">
       <input type="text" onChange={changeHandler} />
