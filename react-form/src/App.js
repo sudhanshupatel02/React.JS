56ef07fd313381e693f5db8e234b66f31dc5b635
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-11-30 20:19:13 +0530

    React Intermediate React Form

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -3,38 +3,55 @@ import { useState } from "react";
 
 function App() {
 
-  const[firstName, setFirstName] = useState("")
-  const[lastName, setLastName] = useState("")
+  // const[firstName, setFirstName] = useState("")
+  // const[lastName, setLastName] = useState("")
 
-  console.log(firstName)
-  console.log(lastName)
-  function changeFirstNameHandler(event) {
-    // console.log("printing first name")
-    // console.log(event.target.value)
-    setFirstName(event.target.value)
-  }
+  // console.log(firstName)
+  // console.log(lastName)
+  // function changeFirstNameHandler(event) {
+  //   console.log("printing first name")
+  //   console.log(event.target.value)
+  //   setFirstName(event.target.value)
+  // }
   
-  function changelastNameHandler(event) {
-    // console.log("printing last name")
-    // console.log(event.target.value)
-    setLastName(event.target.value)
-  } 
+  // function changelastNameHandler(event) {
+  //   console.log("printing last name")
+  //   console.log(event.target.value)
+  //   setLastName(event.target.value)
+  // } 
+
+  const[formData, setFormDate] = useState( {firstName: "", lastName: ""})
+
+  function changeHandler(event) {
+    setFormDate(prevFormDate => {
+      return{
+        ...prevFormDate,
+        [event.target.name] : event.target.value
+      }
+    });
+  }
 
   return (
     <div className="App">
        <form>
+
         <input 
         type="text" 
         placeholder="first name"
-        onChange={changeFirstNameHandler}
+        onChange={changeHandler}
+        name="firstName"
         />
+
         <br/>
         <br/>
+
         <input 
         type="text" 
         placeholder="first name"
-        onChange={changelastNameHandler}
+        onChange={changeHandler}
+        name="lastName"
         />
+
        </form>
     </div>
   );
